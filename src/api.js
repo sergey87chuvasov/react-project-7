3d268cd4b8ac8e95acb1996bfd3717417d0aee40
Sergey87 <serge.techno@gmail.com>
2022-06-21 20:29:22 +0300

    added recipe page

--- src/api.js
+++ src/api.js
@@ -2,21 +2,16 @@ import { API_URL } from './config';
 
 const getMealById = async (mealId) => {
   const response = await fetch(API_URL + 'lookup.php?i=' + mealId);
-
   return await response.json();
 };
 
 const getAllCategories = async () => {
-  const response = await fetch(
-    'https://www.themealdb.com/api/json/v1/1/categories.php'
-  );
-
+  const response = await fetch(API_URL + 'categories.php');
   return await response.json();
 };
 
 const getFilteredCategory = async (catName) => {
-  const response = await fetch(API_URL + 'filter.php?с=' + catName);
-
+  const response = await fetch(API_URL + 'filter.php?c=' + catName);
   return await response.json();
 };
 
--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -23,9 +23,31 @@ function Recipe() {
           <h6>Category: {recipe.strCategory}</h6>
           {recipe.strArea ? <h6>Area: {recipe.strArea}</h6> : null}
           <p>{recipe.strInstructions}</p>
+
+          <table className='centered'>
+            <thead>
+              <tr>
+                <th>Ingredient</th>
+                <th>Measure</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.keys(recipe).map((key) => {
+                if (key.includes('Ingredient') && recipe[key]) {
+                  return (
+                    <tr key={key}>
+                      <td>{recipe[key]}</td>
+                      <td>{recipe[`strMeasure${key.slice(13)}`]}</td>
+                    </tr>
+                  );
+                }
+                return null;
+              })}
+            </tbody>
+          </table>
           {recipe.strYoutube ? (
             <div className='row'>
-              <h5>Video Recipe</h5>
+              <h5 style={{ margin: '2rem 0 1.5rem' }}>Video Recipe</h5>
               <iframe
                 title={id}
                 src={`https://www.youtube.com/embed/${recipe.strYoutube.slice(
